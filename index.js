4c651d3bb1fe51f23679137dd9eb279ceefe0591
Ismael Ramos <ramossilvanismael@gmail.com>
2017-06-26 15:53:52 +0200

    feat(initial commit): added all files from heroku repo

---
+++ index.js
@@ -0,0 +1,68 @@
+var fs = require('fs');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var jsonParser = bodyParser.json();
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(cors());
+
+app.get('/heroesPowers', function(req, res) {
+  res.send(JSON.parse(fs.readFileSync('./heroes-powers.json', 'utf8')));
+});
+
+app.get('/heroes', function(req, res) {
+  res.send(JSON.parse(fs.readFileSync('./heroes.json', 'utf8')));
+});
+
+app.get('/heroes/:id', function(req, res) {
+  var heroes = JSON.parse(fs.readFileSync('./heroes.json', 'utf8'));
+  for(var i = heroes.length - 1; i >= 0; i--) {
+    if(heroes[i].id === req.params.id) {
+      res.send(JSON.stringify(heroes[i]));
+    }
+  }
+  res.status(404).send('Not found');
+});
+
+app.post('/heroes', jsonParser, function (req, res) {
+  if (!req.body) return res.sendStatus(400);
+  var heroes = JSON.parse(fs.readFileSync('./heroes.json', 'utf8'));
+  for(var i = heroes.length - 1; i >= 0; i--) {
+    if(heroes[i].id === req.body.id) {
+      res.status(500).send('Duplicated id');
+      return;
+    }
+  }
+  heroes.push(req.body);
+  fs.writeFileSync('./heroes.json', JSON.stringify(heroes));
+  res.send(heroes);
+});
+
+app.put('/heroes/:id', jsonParser, function (req, res) {
+  var heroes = JSON.parse(fs.readFileSync('./heroes.json', 'utf8'));
+  for(var i = heroes.length - 1; i >= 0; i--) {
+    if(heroes[i].id === req.params.id) {
+      heroes[i] = req.body;
+    }
+  }
+  fs.writeFileSync('./heroes.json', JSON.stringify(heroes));
+  res.send(heroes);
+});
+
+app.delete('/heroes/:id', jsonParser, function (req, res) {
+  var heroes = JSON.parse(fs.readFileSync('./heroes.json', 'utf8'));
+  for(var i = heroes.length - 1; i >= 0; i--) {
+    if(heroes[i].id === req.params.id) {
+      heroes.splice(i, 1);
+    }
+  }
+  fs.writeFileSync('./heroes.json', JSON.stringify(heroes));
+  res.send(heroes);
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
