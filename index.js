f213d083639cd51cc44dee1a8f5c725ccdd13fd5
Ismael Ramos <ramossilvanismael@gmail.com>
2017-07-22 19:41:12 +0200

    feat(heroes): list of heroes from db

--- index.js
+++ index.js
@@ -11,26 +11,25 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(cors());
 
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
+app.get('/heroes', function (req, res) {
+  pg.connect(process.env.DATABASE_URL, function (err, client, done) {
+    client.query('SELECT * FROM heroes', function (err, result) {
       done();
-      if (err)
-      { console.error(err); response.send("Error " + err); }
-      else
-      { response.render('pages/db', {results: result.rows} ); }
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error');
+      }
+      else {
+        res.send(result.rows);
+      }
     });
   });
 });
 
-app.get('/heroes', function(req, res) {
-  res.send(JSON.parse(fs.readFileSync('./heroes.json', 'utf8')));
-});
-
-app.get('/heroes/:id', function(req, res) {
+app.get('/heroes/:id', function (req, res) {
   var heroes = JSON.parse(fs.readFileSync('./heroes.json', 'utf8'));
-  for(var i = 0; i < heroes.length; i++) {
-    if(heroes[i].id === Number(req.params.id)) {
+  for (var i = 0; i < heroes.length; i++) {
+    if (heroes[i].id === Number(req.params.id)) {
       res.send(JSON.stringify(heroes[i]));
       return;
     }
@@ -42,7 +41,7 @@ app.post('/heroes', jsonParser, function (req, res) {
   if (!req.body) return res.sendStatus(400);
   var heroes = JSON.parse(fs.readFileSync('./heroes.json', 'utf8'));
   var newHero = req.body;
-  newHero.id = heroes[heroes.length-1].id + 1;
+  newHero.id = heroes[heroes.length - 1].id + 1;
   newHero.likes = 0;
   newHero.default = false;
   heroes.push(req.body);
@@ -52,8 +51,8 @@ app.post('/heroes', jsonParser, function (req, res) {
 
 app.post('/heroes/:id/like', jsonParser, function (req, res) {
   var heroes = JSON.parse(fs.readFileSync('./heroes.json', 'utf8'));
-  for(var i = 0; i < heroes.length; i++) {
-    if(heroes[i].id === Number(req.params.id)) {
+  for (var i = 0; i < heroes.length; i++) {
+    if (heroes[i].id === Number(req.params.id)) {
       heroes[i].likes += 1;
     }
   }
@@ -65,8 +64,8 @@ app.delete('/heroes/:id', jsonParser, function (req, res) {
   const idToRemove = Number(req.params.id);
 
   var heroes = JSON.parse(fs.readFileSync('./heroes.json', 'utf8'));
-  for(var i = 0; i < heroes.length; i++) {
-    if(heroes[i].id === idToRemove && heroes[i].default) {
+  for (var i = 0; i < heroes.length; i++) {
+    if (heroes[i].id === idToRemove && heroes[i].default) {
       res.status(500).send('Default hero');
       return;
     } else if (heroes[i].id === idToRemove) {
@@ -77,6 +76,6 @@ app.delete('/heroes/:id', jsonParser, function (req, res) {
   res.send(heroes);
 });
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 });
