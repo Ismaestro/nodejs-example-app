e3c01fec5c6e549dfbe1dfbec0049ae9bad8179d
Ismael Ramos <ramossilvanismael@gmail.com>
2017-07-22 19:19:00 +0200

    feat(db): added postgres

--- index.js
+++ index.js
@@ -3,11 +3,25 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var pg = require('pg');
+
 var jsonParser = bodyParser.json();
 
 app.set('port', process.env.PORT || 3000);
 
 app.use(cors());
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+      { console.error(err); response.send("Error " + err); }
+      else
+      { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
 
 app.get('/heroes', function(req, res) {
   res.send(JSON.parse(fs.readFileSync('./heroes.json', 'utf8')));
